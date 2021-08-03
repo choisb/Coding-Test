442954ef02f0f4fee3255feba4505a7cd76be5b9
Seong Beom Choi <s004402@gmail.com>
2021-08-03 23:50:10 +0900

    first commit

---
+++ programmers/p001.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <iostream>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+    string answer = "";
+
+    unordered_map<string, int> ht;
+
+    // 참가자의 value를 ++
+    for (auto p : participant)
+        ht[p]++;
+
+    // 완주자의 value를 --
+    for (auto c : completion)
+        ht[c]--;
+
+    // value가 1인경우 완주하지 못한사람.
+    for (auto h : ht)   
+        if (h.second == 1) 
+            answer = h.first;
+
+    cout << answer << endl;
+    return answer;
+    
+}
