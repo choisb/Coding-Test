0a950acb3e5bfebe4b509beb03fea4e45cdb8700
Seong Beom Choi <s004402@gmail.com>
2021-08-13 12:06:52 +0900

    백준 9184 신나는 함수 실행

---
+++ BOJ/2_Silver/BOJ9184.cpp
@@ -0,0 +1,42 @@
+// 문제 : 신나는 함수 실행 (Silver 2)
+// 출처 : 백준 (https://www.acmicpc.net/problem/9184)
+// 풀이 : https://dev-sbee.tistory.com/ 참고
+
+#include <iostream>
+using namespace std;
+
+int dp[21][21][21] = {};
+
+int w(int a, int b, int c)
+{
+    if (a <= 0 || b <= 0 || c <= 0)
+        return 1;
+
+    if (a > 20 || b > 20 || c > 20)
+        return w(20, 20, 20);
+
+    if (dp[a][b][c] != 0)
+        return dp[a][b][c];
+
+    if (a < b && b < c)
+        return dp[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+
+    return dp[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+}
+
+int main()
+{
+    //입출력 성능향상을 위한 설정
+    ios_base::sync_with_stdio(false);
+    cout.tie(NULL);
+    cin.tie(NULL);
+
+    while (true)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if (a == -1 && b == -1 && c == -1)
+            break;
+        printf("w(%d, %d, %d) = %d\n", a, b, c, w(a, b, c));
+    }
+}
